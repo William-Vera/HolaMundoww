16b6b2caa7165757ea496e690b86b418f8100f0b
TIC - UTEQ <francisco.zambrano2018@uteq.edu.ec>
2023-06-08 16:16:50 -0500

    Avance, habilitar el botón enviar

--- app/src/main/java/com/example/holamundoww/MainActivity.java
+++ app/src/main/java/com/example/holamundoww/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.holamundoww;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void btEnviar(View view)
+    {
+        EditText txtNombre = (EditText)findViewById(R.id.txtNombre1);
+        Toast.makeText(this.getApplicationContext(), "Hello"+txtNombre.getText().toString(), Toast.LENGTH_SHORT).show();
+
+
     }
 }
